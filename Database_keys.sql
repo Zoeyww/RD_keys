4e0824e0613bf6e2ba0f575d594cec187e2c2fb4
armantavanaa <58196629+armantavanaa@users.noreply.github.com>
2021-10-06 08:40:39 -0700

    Update Database_keys.sql

--- Database_keys.sql
+++ Database_keys.sql
@@ -27,24 +27,24 @@ create table course
 );
 	
 ​
-insert into instructor values ('01','Jeff','Hamrick','000-0001','jeff@usfca.edu');
-insert into instructor values ('02', 'Terence','Parr','000-0002','terrence@usfca.edu');
+insert into instructor values ('01', 'Jeff', 'Hamrick', '000-0001', 'jeff@usfca.edu');
+insert into instructor values ('02', 'Terence', 'Parr', '000-0002', 'terrence@usfca.edu');
 
 --------------error, primary key should be unique (same id as previous)-------------------------
-insert into instructor values ('01', 'Michael','Ruddy','000-0003','michael@usfca.edu');
+insert into instructor values ('01', 'Michael', 'Ruddy', '000-0003', 'michael@usfca.edu');
 ------------------------------------------------------------------------------------------------
 
 -------------- error, unqiue key should be unique (same email as previous)----------------------
-insert into instructor values ('03', 'Michael','Ruddy','000-0003','jeff@usfca.edu');
+insert into instructor values ('03', 'Michael', 'Ruddy', '000-0003', 'jeff@usfca.edu');
 ------------------------------------------------------------------------------------------------
 
-insert into instructor values ('03', 'Michael','Ruddy','000-0003','michael@usfca.edu');
+insert into instructor values ('03', 'Michael', 'Ruddy', '000-0003', 'michael@usfca.edu');
 
 
-insert into course values ('msds504','Stats','1','Summer',2021,'01');
-insert into course values ('msds692','Data Acquisition','1','Fall',2021,'02');
-insert into course values ('msds692','Data Acquisition','2','Fall',2021,'02');
-insert into course values ('msds610','Communication','1','Fall',2021,'03');
+insert into course values ('msds504', 'Stats', '1', 'Summer', 2021, '01');
+insert into course values ('msds692', 'Data Acquisition', '1', 'Fall', 2021, '02');
+insert into course values ('msds692', 'Data Acquisition', '2', 'Fall', 2021, '02');
+insert into course values ('msds610', 'Communication', '1', 'Fall', 2021, '03');
 
 -------------- error, instructor table does not have ID'04' right now--------------------------- 
 insert into course values ('msds691', 'Database', '2', 'Fall', 2021, '04'); 
